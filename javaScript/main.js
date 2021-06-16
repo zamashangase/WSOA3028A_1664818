af9c0e5b374c2159a3d389a220419a9bd3c7f459
zamashangase <1664818@students.wits.ac.za>
2021-06-16 17:48:02 +0200

    Update main.js
    script code

---
+++ javaScript/main.js
@@ -0,0 +1,25 @@
+ts<!DOCTYPE ;html>
+<html>
+<head>
+<script>
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "Paragraph changed."
+    }
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "Paragraph changed."
+    }
+    <script>
+        src= "https://zamashangase.github.io/WSOA3028A_1664818/index.html/js/myScript.js"
+    </script>
+    <script src="main.js"></script>
+</script>
+</head>
+<body>
+    <h1>Nandi's Work</h1>
+    <p id= "demo"> This is a website for my character Nandi, who is a part time art consultant and part-time digital art student at Wits University.</p>
+    <button type= "button"
+    onclick="myFunction()"Try It></button>
+    
+
+</body>
+</html>
